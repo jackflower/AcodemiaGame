515c7114f402eee22a4be13019940e3802dc1203
jackflower <jackflower@poczta.onet.pl>
2019-12-06 16:21:47 +0100

    Przygotowanie do wykorzystania gotowych klas. Poruszanie.

--- AcodemiaGame/main.cpp
+++ AcodemiaGame/main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "source/rendering/Texture.h"
+//#include "source/physicals/actor/Actor.h"
+
+
+//#include "source/rendering/Displayable.h"
+//#include "source/animation/Animated.h"
+//#include "source/physicals/physical/Physical.h"
+//#include "source/physicals/bullet/Bullet.h"
+#include "source/physicals/player/Player.h"
+//#include "source/physicals/enemy/Enemy.h"
+#include "source/manager/PhysicalManager.h"
+//#include "source/explosion/Explosion.h"
+//#include "source/enemygenerator/EnemyGenerator.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Acodemia++ SCI 2019-2020 C/C++ and SFML", sf::Style::Close);
 	window.setKeyRepeatEnabled(false);
+
+	// P l a y e r s
+	acodemia::rendering::Texture player_texture;
+	player_texture.loadFromFile("../data/ship.png");
+	player_texture.setSmooth(true);
+
+	Actor *physical;
+	// P l a y e r
+	physical = gPhysicalManager.CreatePlayer();
+	physical->setTexture(player_texture);
+	physical->setUseDisplayable(true);
+	physical->setScale(2);
+	physical->setOrigin(physical->getLocalBounds().width * 0.5f, physical->getLocalBounds().height * 0.5f);
+	physical->setPosition(200, 200);
+	physical->setSpeed(200);
+	physical->setUpsideDown(false);
+
 
 	sf::Clock clock;
 	float time = 0.0f;
@@ -29,8 +59,13 @@ int main()
 		time = clock.restart().asSeconds();
 		//std::cout << time << std::endl;
 
+		//physical->setPosition(physical->getPosition().x + 0.125f, physical->getPosition().y);
+		
+		gPhysicalManager.updatePhysical(time);
+
 		window.clear(sf::Color::White);
 		window.draw(duszek);
+		gPhysicalManager.draw(window);
 		window.display();
 	}
 	return 0;
