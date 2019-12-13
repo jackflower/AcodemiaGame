4b5dd91829e484f317f90b5af24d9dcfdaed0298
jackflower <jackflower@poczta.onet.pl>
2019-12-13 14:31:11 +0100

    Addition of the Tower class - defense / attack functionality - prototype.

---
+++ AcodemiaGame/source/physicals/tower/Tower.cpp
@@ -0,0 +1,58 @@
+﻿////////////////////////////////////////////////////////////
+//
+// Acodemia Copyright (C) Jacek Kwiatek
+// e-mail: jackflower (at) poczta.onet.pl
+// acodemia.pl
+//
+// To oprogramowanie dostarczane jest w postaci takiej,
+// w jakiej jest, bez wyraźnych ani domniemanych gwarancji.
+//
+// W żadnym wypadku Autor nie ponosi odpowiedzialności
+// za jakiekolwiek szkody powstałe w wyniku korzystania
+// z tego oprogramowania.
+//
+// Zezwala się na korzystanie z tego oprogramowania
+// w dowolnym celu, także komercyjnym. Można je zmienić
+// i swobodnie rozpowszechniać.
+//
+// Zastrzega się następujące ograniczenia:
+//
+// 1. Jeśli używasz tego oprogramowania w swoich
+//    produktach, potwierdzenie pochodzenia tego
+//    oprogramowania w dokumentacji produktu,
+//    byłoby docenione, ale nie jest wymagane.
+//
+////////////////////////////////////////////////////////////
+
+#include "Tower.h"
+
+namespace acodemia
+{
+	namespace tower
+	{
+		//Konstruktor domyślny
+		Tower::Tower()
+		{
+			// implementation to do...
+		}
+
+		//Konstruktor kopiujący
+		Tower::Tower(const Tower & copy)
+		{
+			//implementation to do...
+		}
+
+		//Konstruktor przenoszący
+		Tower::Tower(Physical && Tower)
+		{
+			// implementation to do...
+		}
+
+		//Destruktor wirtualny
+		Tower::~Tower()
+		{
+			// implementation to do...
+		}
+
+	}//namespace physical
+}// namespace acodemia
---
+++ AcodemiaGame/source/physicals/tower/Tower.h
@@ -0,0 +1,75 @@
+﻿////////////////////////////////////////////////////////////
+//
+// Acodemia Copyright (C) Jacek Kwiatek
+// e-mail: jackflower (at) poczta.onet.pl
+// acodemia.pl
+//
+// To oprogramowanie dostarczane jest w postaci takiej,
+// w jakiej jest, bez wyraźnych ani domniemanych gwarancji.
+//
+// W żadnym wypadku Autor nie ponosi odpowiedzialności
+// za jakiekolwiek szkody powstałe w wyniku korzystania
+// z tego oprogramowania.Zastrzega
+//
+// Zezwala się na korzystanie z tego oprogramowania
+// w dowolnym celu, także komercyjnym. Można je zmienić
+// i swobodnie rozpowszechniać.
+//
+// Zastrzega się następujące ograniczenia:
+//
+// 1. Jeśli używasz tego oprogramowania w swoich
+//    produktach, potwierdzenie pochodzenia tego
+//    oprogramowania w dokumentacji produktu,
+//    byłoby docenione, ale nie jest wymagane.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef H_TOWER_ACODEMIA
+#define H_TOWER_ACODEMIA
+
+#include "../physical/Physical.h"
+using namespace acodemia::physical;
+
+namespace acodemia
+{
+	namespace tower
+	{
+		///
+		///Klasa reprezentuje wieżę - atak/obrona
+		///
+		class Tower:public Physical
+		{
+
+		public:
+
+			///
+			///Konstruktor domyślny
+			///
+			Tower();
+
+			///
+			///Konstruktor kopiujący
+			///
+			///@param copy - stała referencja na obiekt klasy Tower
+			///
+			Tower(const Tower & copy);
+
+			///
+			///Konstruktor przenoszący
+			///
+			///@param other = referencja do r-wartości
+			///
+			Tower(Physical && Tower);
+
+			///
+			///Destruktor wirtualny
+			///
+			virtual ~Tower();
+
+		protected:
+		private:
+
+		};
+	}//namespace physical
+}// namespace acodemia
+#endif//H_TOWER_ACODEMIA
