1c2cdb3d163a3d91c40566a696377b2e8d88150b
jackflower <jackflower@poczta.onet.pl>
2020-02-21 13:44:34 +0100

    Update - dotyczy zajęć 29 lutego 2020

--- AcodemiaGame/main.cpp
+++ AcodemiaGame/main.cpp
@@ -1,44 +1,102 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include "source/rendering/Texture.h"
+#include "source/rendering/Displayable.h"
+#include "source/animation/Animated.h"
+#include "source/physicals/physical/Physical.h"
+#include "source/physicals/bullet/Bullet.h"
 #include "source/physicals/player/Player.h"
+#include "source/physicals/enemy/Enemy.h"
 #include "source/manager/PhysicalManager.h"
-
+#include "source/explosion/Explosion.h"
+#include "source/enemygenerator/EnemyGenerator.h"
 
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Acodemia++ SCI 2019-2020 C/C++ and SFML", sf::Style::Close);
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Acodemia++ SCI 2019-2020", sf::Style::Close);
 	window.setKeyRepeatEnabled(false);
+
+	// G r a p h i c s
+
+	// E x p l o s i o n
+	acodemia::rendering::Texture explosion_texture;
+	explosion_texture.loadFromFile("../data/explosion.png");
+	explosion_texture.setSmooth(true);
+
+	// B u l l e t
+	acodemia::rendering::Texture bullet_texture;
+	bullet_texture.loadFromFile("../data/bullet.png");
+	bullet_texture.setSmooth(true);
 
 	// P l a y e r s
 	acodemia::rendering::Texture player_texture;
-	player_texture.loadFromFile("../data/ship.png");
+	//player_texture.loadFromFile("../data/ship.png");
+	player_texture.loadFromFile("../data/ship_x.png");
 	player_texture.setSmooth(true);
+
+	//Enemies
+	acodemia::rendering::Texture enemy_texture;
+	enemy_texture.loadFromFile("../data/enemy.png");
+	enemy_texture.setSmooth(true);
 
 	Actor *physical;
 	// P l a y e r
 	physical = gPhysicalManager.CreatePlayer();
 	physical->setTexture(player_texture);
 	physical->setUseDisplayable(true);
+	physical->setBulletTexture(&bullet_texture);
+	physical->setBulletSpeed(300.f);
+	physical->setBulletMotion(sf::Vector2f(0, -1));
+	physical->setBulletCaliber(3.0f);
+	physical->setBulletLifeTime(4.125f);
+	physical->setGunRate(0.5f);
 	physical->setScale(2);
 	physical->setOrigin(physical->getLocalBounds().width * 0.5f, physical->getLocalBounds().height * 0.5f);
 	physical->setPosition(200, 200);
-	physical->setSpeed(200);
+	physical->setSpeed(100);
 	physical->setUpsideDown(false);
+	//dane eksplozji...
+	physical->setExplosionTexture(&explosion_texture);
+	physical->setExplosionFrameSize(64, 64);
+	physical->setExplosionSpeed(0.25f);
+
+	// E n e m i e s
+	physical = gPhysicalManager.CreateEnemy();
+	physical->setTexture(enemy_texture);
+	physical->setUseDisplayable(true);
+	physical->setBulletTexture(&bullet_texture);
+	physical->setBulletSpeed(400.f);
+	physical->setBulletMotion(sf::Vector2f(0, 1));
+	physical->setBulletCaliber(2.0f);
+	physical->setBulletLifeTime(3.25f);
+	physical->setGunRate(2.25f);
+	physical->setOrigin(physical->getLocalBounds().width * 0.5f, physical->getLocalBounds().height * 0.5f);
+	physical->setPosition(500, -170);
+	//physical->setPosition(0, 0);
+	physical->setSpeed(50);
+	physical->setScale(1, -1);
+	physical->setUpsideDown(true);
+	//dane eksplozji...
+	physical->setExplosionTexture(&explosion_texture);
+	physical->setExplosionFrameSize(64, 64);
+	physical->setExplosionSpeed(1.f);
 
 
 	sf::Clock clock;
 	float time = 0.0f;
 
-	sf::Texture obrazek;
-	obrazek.loadFromFile("../data/ship.png");
-	obrazek.setSmooth(true);
-	sf::Sprite duszek;
-	duszek.setTexture(obrazek);
-	duszek.setPosition(400, 300);
+	//Animation  u s a g e  -  logo SCI
+	acodemia::rendering::Texture animation_texture;
+	animation_texture.loadFromFile("../data/logo_sci_atlas.png");
+	acodemia::animation::Animation animacja;
+	animacja.setAnimationFramesFromAtlasTexture(animation_texture, 50, 50);
+	acodemia::animation::Animated animek;
+	animek.setAnimation(&animacja);
+	animek.setPosition(700.f, 100.f);
+	animek.setAnimationSpeed(1.5f);
+	//Animation  u s a g e  -  logo SCI
 
-	//pêtla czasu rzeczywistego...
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -49,14 +107,11 @@ int main()
 		}
 
 		time = clock.restart().asSeconds();
-		//std::cout << time << std::endl;
 
-		//physical->setPosition(physical->getPosition().x + 0.125f, physical->getPosition().y);
-		
+		animek.update(time);
 		gPhysicalManager.updatePhysical(time);
-
 		window.clear(sf::Color::White);
-		window.draw(duszek);
+		animek.draw(&window);
 		gPhysicalManager.draw(window);
 		window.display();
 	}
--- Testowy/main.cpp
+++ Testowy/main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <fstream>
 int main()
 {
+	std::fstream plik;
+	plik.open("zorro.txt", std::ios::out);
+	plik << "-----" << std::endl;
+	plik << "-----" << std::endl;
+	plik << "Jacek" << std::endl;
+	plik << "-----" << std::endl;
+	plik << "-----" << std::endl;
+	plik << "-----" << std::endl;
+
+	//std::cout << "Jacek" << std::endl;
+	
+	getchar();
 	return 0;
 }
